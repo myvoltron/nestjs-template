0b1d4bf6a00b2ed99e58e71af07127ff1d5e1ad8
myvoltron <leejs8266@gmail.com>
2023-04-08 21:51:50 +0900

    feat: winston 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { CONFIG_OPTIONS_PROVIDER } from './common/config/config-options.service';
 import { TypeOrmConfigService } from './common/config/typeorm-config.service';
+import { WinstonConfigService } from './common/logger/winston-config.service';
 
 @Module({
   imports: [
@@ -11,6 +13,10 @@ import { TypeOrmConfigService } from './common/config/typeorm-config.service';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
+    }),
+    WinstonModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: WinstonConfigService,
     }),
   ],
   controllers: [AppController],
