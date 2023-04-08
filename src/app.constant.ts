9da9b44489e2bfc0b6b49954abe5815b92e0baa7
myvoltron <leejs8266@gmail.com>
2023-04-08 21:51:35 +0900

    chore: env 변수 추가

---
+++ src/app.constant.ts
@@ -0,0 +1,5 @@
+export enum ENV {
+  Local = 'LOCAL',
+  Develop = 'DEVELOP',
+  Production = 'PRODUCTION',
+}
--- src/common/config/config-options.service.ts
+++ src/common/config/config-options.service.ts
@@ -3,6 +3,8 @@ import * as Joi from 'joi';
 export const CONFIG_OPTIONS_PROVIDER = {
   isGlobal: true,
   validationSchema: Joi.object({
+    PROJECT_NAME: Joi.string().required(),
+    ENV: Joi.string().required(),
     APP_PORT: Joi.number().required(),
     DB_HOST: Joi.string().required(),
     DB_PORT: Joi.number().required(),
