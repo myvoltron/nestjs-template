b0ccadc4df5d52e5cb0ac2e926591c53009f3fe2
leejs7856 <leejs8266@gmail.com>
2023-07-13 16:01:55 +0900

    docs: AppController 문서화

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { AppPath } from './app.constant';
 
 @Controller()
 export class AppController {
+  @ApiOperation({
+    summary: '서버가 제대로 작동되는지 확인하기 위한 api입니다.',
+  })
+  @ApiOkResponse()
   @Get(AppPath.HeartBeat)
   getHeartbeat() {
     return;
