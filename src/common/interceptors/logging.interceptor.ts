8a5f9df2549e3ec88521aeb48498a2d665edca68
myvoltron <leejs8266@gmail.com>
2023-07-18 23:07:56 +0900

    feat: logging interceptor class 작성 중
    - 우선 껍데기만 작성 추후에 winston logger가 완성되면 구현될 예정

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -1,3 +1,16 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
 /**
  * @todo api request와 response를 로그로 남기는 interceptor 클래스를 추가한다.
  */
+export class LoggingInterceptor implements NestInterceptor {
+  constructor() {}
+
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+
+    return;
+  }
+}
