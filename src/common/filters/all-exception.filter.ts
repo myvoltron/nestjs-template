6cf7bb19f8edf092c5106558422c2fc957bfcafa
myvoltron <leejs8266@gmail.com>
2023-07-24 23:18:40 +0900

    feat: 에러 로깅할 때 센트리 capture 작동

--- src/common/filters/all-exception.filter.ts
+++ src/common/filters/all-exception.filter.ts
@@ -1,4 +1,5 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
 import { Response } from 'express';
 import { ExtendedRequest } from '../logger/extended-request';
 import { WinstonLogger } from '../logger/winston-logger.serivce';
@@ -16,8 +17,9 @@ export class AllExceptionFilter implements ExceptionFilter {
 
     const statusCode = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const loggingObject = this.logger.createLoggingObject(request, response);
-    this.logger.error(JSON.stringify(loggingObject));
+    const loggingObject = JSON.stringify(this.logger.createLoggingObject(request, response));
+    this.logger.error(loggingObject);
+    Sentry.captureException(loggingObject);
 
     response.status(statusCode).json({
       statusCode,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
 import { AppModule } from './app.module';
 import { setupDocument } from './common/docs/setup-document';
 import { WinstonLogger } from './common/logger/winston-logger.serivce';
@@ -8,6 +9,10 @@ import { WinstonLogger } from './common/logger/winston-logger.serivce';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  Sentry.init({
+    dsn: configService.get<string>('SENTRY_DSN'),
+  });
 
   app.useLogger(app.get<WinstonLogger>(WinstonLogger));
   app.useGlobalPipes(new ValidationPipe());
